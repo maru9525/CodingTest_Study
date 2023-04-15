f78bef5e6773e13a550bc6662669e333e8b28c11
hwangjeunghwan <j_h6467@naver.com>
2023-04-15 23:50:05 +0900

    Chapter8_11~14

--- 정환/Chapter8_BFSDFS/Chapter8_13.java
+++ 정환/Chapter8_BFSDFS/Chapter8_13.java
@@ -1,2 +1,48 @@
-package CodingTest_Study.정환.Chapter8_BFSDFS;public class Chapter8_13 {
+package CodingTest_Study.정환.Chapter8_BFSDFS;
+
+import java.util.Scanner;
+
+public class Chapter8_13 {
+    static int[] dx = {-1,0,1,0,-1,1,-1,1};
+    static int[] dy = {0,1,0,-1,-1,1,1,-1};
+    static int[][] array;
+    static boolean[][] check;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        array = new int[n][n];
+        check = new boolean[n][n];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++)
+                array[i][j] = sc.nextInt();
+        }
+        int result = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++) {
+                if(array[i][j] == 1){
+                    dfs(i,j,array,check);
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+
+
+    }
+
+    static void dfs(int i, int j, int[][] array, boolean[][] check){
+        check[i][j] = true;
+        for(int k=0; k<8; k++){
+            int nx = dx[k]+i;
+            int ny = dy[k]+j;
+            if(nx<0||ny<0 || nx>array.length-1 || ny > array.length-1) continue;
+            if(check[nx][ny]) continue;
+            if(array[nx][ny] ==0) continue;
+            check[nx][ny]=true;
+            array[nx][ny] = 0;
+            dfs(nx, ny, array, check);
+        }
+    }
 }
--- 정환/Chapter8_BFSDFS/Chapter8_14.java
+++ 정환/Chapter8_BFSDFS/Chapter8_14.java
@@ -1,2 +1,63 @@
-package CodingTest_Study.정환.Chapter8_BFSDFS;public class Chapter8_14 {
+package CodingTest_Study.정환.Chapter8_BFSDFS;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Chapter8_14 {
+    //강의봤습니다..
+    static class point{
+        int x;
+        int y;
+        point(int x, int y){
+            this.x = x;
+            this.y =y;
+        }
+    }
+
+    static List<point> pz, hs;
+    static int[] combi;
+    static int result = Integer.MAX_VALUE;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        pz = new ArrayList<>();
+        hs = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++) {
+                int a = sc.nextInt();
+                if(a==1){
+                    hs.add(new point(i, j));
+                }
+                if(a==2){
+                    pz.add(new point(i, j));
+                }
+            }
+        }
+        combi = new int[m];
+        dfs(m, 0, 0, pz.size());
+
+        System.out.println(result);
+    }
+
+    static void dfs(int m, int L, int s, int len){
+        if(L == m){
+            int sum = 0;
+            for(point p : hs){
+                int dis =  Integer.MAX_VALUE;
+                for(int i : combi){
+                    dis = Math.min(dis, (Math.abs(p.x-pz.get(i).x) + Math.abs(p.y-pz.get(i).y)));
+                }
+                sum += dis;
+            }
+            result = Math.min(sum, result);
+        }else{
+            for(int i=s; i< len; i++){
+                combi[L] = i;
+                dfs(m,L+1, i+1, len);
+            }
+        }
+    }
+
 }
